7296abc1d9d39fcb1698094d7ecb3a2699413add
Fabio Lucas <fabiolucs19@gmail.com>
2022-11-12 15:38:16 -0300

    add third test

--- TestesAutomatizados/src/main/java/org/example/Main.java
+++ TestesAutomatizados/src/main/java/org/example/Main.java
@@ -1,8 +1,7 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver" , "src\\driver\\chromedriver.exe");
-
+    public static void main(String[] args) {     
+        //System.out.println("Executando testes");
     }
 }
--- TestesAutomatizados/src/test/java/TestsWithSelenium.java
+++ TestesAutomatizados/src/test/java/TestsWithSelenium.java
@@ -1,26 +1,20 @@
-import org.asynchttpclient.util.Assertions;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class TestsWithSelenium {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @Before
-    public void makeWebDriverInstance() {
+    public void makeWebDriver() {
+        System.setProperty("chromedriver.exe" , "src\\driver\\chromedriver.exe");
         this.driver = new ChromeDriver();
-    }
-
-    public void openWebSite() {
-
+        driver.manage().window().maximize();
+        driver.get("https://www.kabum.com.br/");
     }
 
     @After
@@ -29,21 +23,27 @@ public class TestsWithSelenium {
         this.driver.quit();
     }
 
-
     @Test
     public void openKabumSiteTest() {
-
-        driver.manage().window().maximize();
-        driver.get("https://www.kabum.com.br/");
         Assert.assertEquals("https://www.kabum.com.br/", driver.getCurrentUrl());
+    }
+
+    @Test
+    public void checkignTitlePageFalsetest() {
+        String src = driver.getTitle();
+        Assert.assertFalse(src == "Kabum");
+        // Name Page is KaBuM! | Maior E-commerce de Tecnologia e Games da América
+        // Latina
     }
 
     @Test
     public void searchExpectedOnKabumPageTest() {
         String searchTerm = "Xbox Series S".toLowerCase();
-        driver.get("https://www.kabum.com.br/");
         driver.findElement(By.xpath("//*[@id=\"input-busca\"]")).sendKeys(searchTerm, Keys.ENTER);
-        String result = driver.findElement(By.xpath("//*[@id=\"listing\"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/button/div/h2/span")).getText().toLowerCase();
+        String result = driver
+                .findElement(
+                        By.xpath("//*[@id=\"listing\"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/button/div/h2/span"))
+                .getText().toLowerCase();
         boolean resultTest = false;
 
         if (result.contains(searchTerm)) {
@@ -53,16 +53,4 @@ public class TestsWithSelenium {
         Assert.assertTrue(resultTest);
     }
 
-//    @Test
-//    public void searchNotExpectedKabumPageTest() {
-//        driver.manage().window().maximize();
-//        driver.get("https://www.kabum.com.br/");
-//        Actions actions = new Actions(driver);
-//        WebElement lancamentosButton = driver.findElement(By.xpath("//*[@id=\"lancamentosMenuSuperior\"]"));
-//
-//        actions.moveToElement(lancamentosButton).perform();
-//
-//
-//
-//    }
 }
